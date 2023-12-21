094918b3478d63543c71c0787db800a243184a56
YoungseopLee <dldudtjqlol@gmail.com>
2023-12-21 11:00:03 +0900

    HelloWorld

---
+++ src/main/java/com/openai/team01/TypePickApplication.java
@@ -0,0 +1,14 @@
+package com.openai.team01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypePickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypePickApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/openai/team01/TypePickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openai.team01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypePickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
